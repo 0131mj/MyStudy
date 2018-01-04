1c48257e2c732e28fee1c105d3678d2ccdd6b544
0131mj <0131mj@gmail.com>
2018-01-04 22:41:49 +0900

    변수만으로 출력 가능하도록 수정

--- php/gugu.php
+++ php/gugu.php
@@ -1,25 +1,30 @@
 <?php
-    function product($x, $y){
-        $result = $x*$y;
-        echo $x.'*'.$y.'='.$result.'&emsp;'; // 곱셈 출력
-    }
 
-    $loop = 0;
+$dan = 2;       //시작단
+$dan_end = 9;   //끝나는 단
+$gob = 1;       //시작하는 곱
+$gob_end = 3;   //끝나는 곱
+$col = 3;       //줄 수
 
-    for ($k=1; $k<4; $k++)
+function product($x, $y)
+{
+    $result = $x * $y;
+    echo $x.'*'.$y.'='.$result.'&emsp;'; // 곱셈 출력
+}
+
+while($dan < $dan_end)
+{
+    for ($j = $gob; $j <= $gob_end; $j++)
     {
-        for ($j=1; $j<4; $j++)
+        for ($i = $dan; $i < $dan+$col; $i++)
         {
-            for ($i = $loop; $i < $loop+3; $i++)
+            if($i <= $dan_end)
             {
-                if($i<8)
-                {
-                    product($i + 2, $j);
-                }
+                product($i, $j);
             }
-            echo '<br>';
         }
         echo '<br>';
-        $loop = $loop+3;
     }
-?>
+    echo '<br>';
+    $dan = $dan+$col;
+}
